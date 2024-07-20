b878956d5b44480ca20e8c5ba4b8085ad8555cd5
Milica Jovicic <165071059+jmilicaa@users.noreply.github.com>
2024-07-20 13:47:10 +0200

    Uspesno detektovanje reci do 5 karaktera
    Kod za datektovanje, dekodiranje i plotovanje reci do 5 karaktera, primeri uspesno dekodovanog signala.

--- DDD_PronalazenjePeakovaITrajanajPrenosaIzmedju.py
+++ DDD_PronalazenjePeakovaITrajanajPrenosaIzmedju.py
@@ -11,6 +11,47 @@ import matplotlib.pyplot as plt
 import scipy
 
 
+def to_decimal(x):
+    a=1
+    rez=0
+    for i in range(0, len(x)):
+        if x[i]=='1':
+            rez+=a
+        a*=2
+    return rez
+
+def find_encoded_word(bits):
+    # Convert the bit sequence to a string for easier manipulation
+    bit_string = ''.join(map(str, bits))
+    bit_string = '0' + bit_string + '111'
+    
+    #print(bit_string)
+    # Length of a complete block
+    block_length = 12
+    
+    longest_word = []
+
+    # Loop over the bit string to find the pattern
+    for start in range(len(bit_string) - block_length + 1):
+        current_word = []
+        #print(start)
+        for i in range(start,len(bit_string) - block_length + 1, 12):
+            print(start, i, bit_string[i], bit_string[i+9:i+12])
+            if bit_string[i]!='0':
+                break
+            if bit_string[i+9:i+12]!='111':
+                break
+            current_word.append(bit_string[i+1:i+9])
+        if len(current_word) > len(longest_word):
+            longest_word=current_word
+
+    print(longest_word)    
+    final_string=""
+    for letter in longest_word:
+        final_string+= chr(to_decimal(letter))
+    return final_string
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect(('192.168.50.60', 3000))
 
@@ -30,14 +71,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     data_ = []
 
     #krece da cita 2. i 3. bit jer prva dva nose informaciju o granicama 
-    for c1, c2 in zip(data[10::2], data[11::2]):
+    for c1, c2 in zip(data[4::2], data[5::2]):
         data_.append(c1 + 2**8 * c2)
 
     #d je broj poslatih informacije 1520+2
     d=len(data_)
     
     #print("***RAW DATA***")
-
+    print(data_)
     data_s=[]
     for i in range(d):
         data_s.append(data_[i])
@@ -66,20 +107,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         numeracija.append(i)
 
     for i in range(d):
-        if(data_[i]>1.8 and data_[i]<3):
+        if(data_[i]>1.8 and data_[i]<3.2):
             data_[i]=2.5
        
-    #print(data_)
-
+    print(data_)
     pikovi=[]
     j=0
     i=0
     while(i<d):
         if(data_[i]!=2.5):
-            start=i
             j=i
             num_of_prev=0
-            while(num_of_prev!=4):
+            while(num_of_prev!=5 and j<d):
                 if(data_[j]==2.5):
                     num_of_prev=num_of_prev+1
                 else:
@@ -89,8 +128,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             i=j
         else:
             i=i+1
-    
-    #print(pikovi)
+    print(pikovi)
 
 
     #data_=np.array(data_)
@@ -134,14 +172,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         pozicije.append([p1,p2])
 
 
-    #print("***PAROVI[MAKSIMALANI_POZITIVNI_PIK,MAKSIMALNI_NEGATIVNI_PIK]***")
-    #print(rastojanja)
-    #print("***PAROVI[POZICIJA_MPP,POZICIJA_MNP]***")
-    #print(pozicije)
+    print("***PAROVI[MAKSIMALANI_POZITIVNI_PIK,MAKSIMALNI_NEGATIVNI_PIK]***")
+    print(rastojanja)
+    print("***PAROVI[POZICIJA_MPP,POZICIJA_MNP]***")
+    print(pozicije)
 
     #znam da moze od jednom ali ovako je lakse za debagovanje raw date
     #print("***DATA SA IZBRISANIM MANJIM PIKOM***")
     for mesto in pozicije:
+        print(mesto)
         if(data_[mesto[0]]-2.5>2.5-data_[mesto[1]]):
             data_[mesto[1]]='a'
         else:
@@ -168,13 +207,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     visina=[]
     k=1
     kombo=[]
+    if (rastojanja[0][0]>rastojanja[0][1]):
+        if(duzina[0]>=19):
+            kombo.append([0,duzina[0]])
+    else:
+        if(duzina[1]>=19):
+            kombo.append([1,duzina[1]])
+    
     for par in rastojanja:
         if(par[0]>par[1]):
             #print(duzina[k])
             #k=k+1
             #print("UP")
             if(k<len(duzina)):
-                kombo.append([1,duzina[k]])
+                if(duzina[k]>=19):
+                    kombo.append([1,duzina[k]])
             k=k+1
             visina.append("UP")
         else:
@@ -182,15 +229,61 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             #k=k+1
             #print("DOWN")
             if(k<len(duzina)):
-                kombo.append([0,duzina[k]])
+                if(duzina[1]>=19):
+                    kombo.append([0,duzina[k]])
             k=k+1
             visina.append("DOWN")
-
+    #print(kombo)
     #print(data_)
-    print(kombo)
+    patern1=[]
+    patern2=[]
+    for i in range(len(kombo)):
+        if (i%2==1):
+            patern1.append(0)
+        else:
+            patern1.append(1)
+    for i in range(len(kombo)):
+        if (i%2==1):
+            patern2.append(1)
+        else:
+            patern2.append(0)
+    
+
+
+    ok=0
+    for i in range(len(kombo)):
+        if (kombo[i][1]>190):
+            if (patern1[i]==1):
+                for j in range(len(kombo)):
+                    kombo[j][0]=patern1[j]
+            else:
+                for j in range(len(kombo)):
+                    kombo[j][0]=patern2[j]
+            ok=1
+
+    if (ok!=1):
+        greska1=0
+        greska2=0
+        for i in range(len(kombo)):
+            if (kombo[i][0]!=patern1[i]):
+                greska1=greska1+1
+
+        for i in range(len(kombo)):
+            if (kombo[i][0]!=patern2[i]):
+                greska2=greska1+2
+        print(greska2,greska1)
+        if (greska1>greska2):
+            for i in range(len(kombo)):
+                kombo[i][0]=patern1[i]
+        else:
+            for i in range(len(kombo)):
+                kombo[i][0]=patern2[i]
 
     #print("PROMENA SMERA UP - SA 0 NA 1 ---- DOWN - SA 1 NA 0")
-    #print(visina)            
+    #print(visina)
+
+
+    print(kombo)            
 
     #SADA IZ DATA_ OSTAVIMO SAMO VECI PIK
 
@@ -200,9 +293,67 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     
     #print(data_)
 
-    plt.plot(numeracija,data_)
+    rezultat=[]
+    for par in kombo:
+        brpn=par[1]/19.9
+        i=1
+        if(brpn<6):
+            if(brpn>5):
+                brpn=par[i]/20.2
+            while (i<brpn):
+                rezultat.append(par[0])
+                i=i+1
+        else:
+            print(brpn)
+            print(par[1]/21.0)
+            if(par[1]/21.0>6):
+                while(i<par[1]/21):
+                    rezultat.append(par[0])
+                    i=i+1
+            else:
+                while(i<=5):
+                    rezultat.append(par[0])
+                    i=i+1
+            
+        
+    print(rezultat)
+
+    rec=find_encoded_word(rezultat)
+
+    print(rec)
+    r=[]
+    for i in kombo:
+        j=0
+        while(j<i[1]):
+            if(i[0]==1):
+                r.append(5)
+            else:
+                r.append(0)
+            j=j+1
+        
+    numeracija2=[]
+    for i in range(len(r)):
+        numeracija2.append(i)
+    plt.plot(numeracija2,r)
 
 
+    p=[]
+    for par in kombo:
+        j=0
+        if (par[0]==1):
+            p.append(4)
+        else:
+            p.append(1)
+        while(j<par[1]-1):
+            p.append(2.5)
+            j=j+1
+
+    numeracija3=[]
+    for i in range(len(p)):
+        numeracija3.append(i)
+    
+    plt.plot(numeracija3,p)
+    plt.title(rec)
 
 
     zeros=0
@@ -214,5 +365,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.close()
 
 
-plt.savefig("DDD_DetekcijaPEakova_2.png")
+print("plotuje ga")
+plt.savefig("test.png")
+#plt.savefig("DDD_sava_dekodovano.png")
  
